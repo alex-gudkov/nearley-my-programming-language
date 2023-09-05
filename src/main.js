4ae1d3b01b1e89553483cb6942c0dde7ac56588a
alex-gudkov <gudkov.bsuir@gmail.com>
2023-09-05 10:30:36 +0300

    take parsing out to INPUT/ and OUTPUT/ folders

--- src/main.js
+++ src/main.js
@@ -1,11 +1,24 @@
 const nearley = require('nearley');
 const grammar = require('./grammar/grammar');
+const path = require('path');
+const fs = require('fs');
 
-// Create a Parser object from our grammar.
+// create a Parser object from grammar
 const parser = new nearley.Parser(nearley.Grammar.fromCompiled(grammar));
 
-// Parse something!
-parser.feed('foo\n');
+// read input data
+const inputFileName = 'main.mypl';
+const inputFilePath = path.join(__dirname, '..', 'input', inputFileName);
+const inputFileData = fs.readFileSync(inputFilePath, { encoding: 'utf8' });
 
-// parser.results is an array of possible parsings.
-console.log(JSON.stringify(parser.results)); // [[[[["foo"],"\n"]]]]
+// parse data
+parser.feed(inputFileData);
+
+const parseResult = parser.results;
+
+// write output data
+const outputFileName = 'main.pr';
+const outputFilePath = path.join(__dirname, '..', 'output', outputFileName);
+const outputFileData = JSON.stringify(parseResult, null, '  ') + '\n';
+
+fs.writeFileSync(outputFilePath, outputFileData, { encoding: 'utf-8' });
