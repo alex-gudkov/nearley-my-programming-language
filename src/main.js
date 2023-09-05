68cda7b81bdabd7cc7bc480e48122e89eab262ae
alex-gudkov <gudkov.bsuir@gmail.com>
2023-09-05 10:04:12 +0300

    refactor project structure

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 const nearley = require('nearley');
-const grammar = require('./grammar.js');
+const grammar = require('./grammar/grammar');
 
 // Create a Parser object from our grammar.
 const parser = new nearley.Parser(nearley.Grammar.fromCompiled(grammar));
