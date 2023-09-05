2addeb62bfc14fb46d457e590d8afe5386cebcb4
alex-gudkov <gudkov.bsuir@gmail.com>
2023-09-05 11:04:54 +0300

    refactor project code

--- src/main.js
+++ src/main.js
@@ -3,24 +3,28 @@ const grammar = require('./grammar/grammar');
 const path = require('path');
 const fs = require('fs');
 
-const FILE_NAME = 'main';
+const FILE_NAME = 'program';
 
-// create a Parser object from grammar
-const parser = new nearley.Parser(nearley.Grammar.fromCompiled(grammar));
+function main() {
+  // create a Parser object from grammar
+  const parser = new nearley.Parser(nearley.Grammar.fromCompiled(grammar));
 
-// read input data
-const inputFileName = FILE_NAME + '.mypl';
-const inputFilePath = path.join(__dirname, '..', 'input', inputFileName);
-const inputFileData = fs.readFileSync(inputFilePath, { encoding: 'utf8' });
+  // read input data
+  const inputFileName = FILE_NAME + '.mypl';
+  const inputFilePath = path.join(__dirname, '..', 'input', inputFileName);
+  const inputFileData = fs.readFileSync(inputFilePath, { encoding: 'utf8' });
 
-// parse data
-parser.feed(inputFileData);
+  // parse data
+  parser.feed(inputFileData);
 
-const parseResult = parser.results;
+  const parseResult = parser.results;
 
-// write output data
-const outputFileName = FILE_NAME + '.pr';
-const outputFilePath = path.join(__dirname, '..', 'output', outputFileName);
-const outputFileData = JSON.stringify(parseResult, null, '  ') + '\n';
+  // write output data
+  const outputFileName = FILE_NAME + '.pr';
+  const outputFilePath = path.join(__dirname, '..', 'output', outputFileName);
+  const outputFileData = JSON.stringify(parseResult, null, '  ') + '\n';
 
-fs.writeFileSync(outputFilePath, outputFileData, { encoding: 'utf-8' });
+  fs.writeFileSync(outputFilePath, outputFileData, { encoding: 'utf-8' });
+}
+
+main();
