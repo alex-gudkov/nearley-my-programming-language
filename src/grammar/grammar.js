f16cc6b76eff3f6b7a417bf502dbcda07e50b525
alex-gudkov <gudkov.bsuir@gmail.com>
2023-09-05 17:39:24 +0300

    grammar: add print statement

--- src/grammar/grammar.js
+++ src/grammar/grammar.js
@@ -6,11 +6,19 @@ var grammar = {
     Lexer: undefined,
     ParserRules: [
     {"name": "program", "symbols": ["variableAssignment"], "postprocess": id},
-    {"name": "program", "symbols": ["number"], "postprocess": id},
+    {"name": "program", "symbols": ["printStatement"], "postprocess": id},
+    {"name": "printStatement$string$1", "symbols": [{"literal":"P"}, {"literal":"R"}, {"literal":"I"}, {"literal":"N"}, {"literal":"T"}], "postprocess": function joiner(d) {return d.join('');}},
+    {"name": "printStatement", "symbols": ["printStatement$string$1", "__", "number", "_", {"literal":";"}], "postprocess": 
+        // "PRINT 10;" -> d = [ "PRINT", null, "10", null, ";" ]
+        (d) => ({
+          type: "PrintStatement",
+          value: d[2]
+        })
+          },
     {"name": "variableAssignment$string$1", "symbols": [{"literal":"V"}, {"literal":"A"}, {"literal":"R"}], "postprocess": function joiner(d) {return d.join('');}},
     {"name": "variableAssignment$string$2", "symbols": [{"literal":"A"}, {"literal":"S"}, {"literal":"S"}, {"literal":"I"}, {"literal":"G"}, {"literal":"N"}], "postprocess": function joiner(d) {return d.join('');}},
     {"name": "variableAssignment", "symbols": ["variableAssignment$string$1", "__", "identifier", "__", "variableAssignment$string$2", "__", "number", "_", {"literal":";"}], "postprocess": 
-        // "VAR x ASSIGN 123.456;" -> d = [ "VAR", null, "x", null, "ASSIGN", null, "123.456", null, ";" ]
+        // "VAR x ASSIGN 10;" -> d = [ "VAR", null, "x", null, "ASSIGN", null, "10", null, ";" ]
         (d) => ({
           type: "VariableAssignment",
           identifier: d[2],
--- src/grammar/grammar.ne
+++ src/grammar/grammar.ne
@@ -1,11 +1,19 @@
 program
   -> variableAssignment {% id %}
-  |  number             {% id %}
+  |  printStatement     {% id %}
+
+printStatement
+  -> "PRINT" __ number _ ";" {%
+    // "PRINT 10;" -> d = [ "PRINT", null, "10", null, ";" ]
+    (d) => ({
+      type: "PrintStatement",
+      value: d[2]
+    })
+  %}
 
 variableAssignment
-  -> "VAR" __ identifier __ "ASSIGN" __ number _ ";"
-  {%
-    // "VAR x ASSIGN 123.456;" -> d = [ "VAR", null, "x", null, "ASSIGN", null, "123.456", null, ";" ]
+  -> "VAR" __ identifier __ "ASSIGN" __ number _ ";" {%
+    // "VAR x ASSIGN 10;" -> d = [ "VAR", null, "x", null, "ASSIGN", null, "10", null, ";" ]
     (d) => ({
       type: "VariableAssignment",
       identifier: d[2],
@@ -20,14 +28,13 @@ number
     // "123.456" -> d = [ "123", ".", "456" ]
     (d) => Number(d[0] + "." + d[2])
   %}
-  | digits {%
+  |  digits {%
     // "123" -> d = [ "123" ]
     (d) => Number(d[0])
   %}
 
 digits
-  -> [0-9]:+
-  {%
+  -> [0-9]:+ {%
     // "123" -> d = [ [ "1", "2", "3" ] ]
     (d) => d[0].join("")
   %}
