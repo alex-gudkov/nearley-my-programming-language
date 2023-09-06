007e94a767bb206b0cac79b980eb0162c07c920b
alex-gudkov <gudkov.bsuir@gmail.com>
2023-09-06 15:12:09 +0300

    divide MyPL/AST/JS files into folders && test PRINT statement generation

--- src/main.js
+++ src/main.js
@@ -3,38 +3,121 @@ const { readFile, readdir, writeFile } = require('node:fs/promises');
 const nearley = require('nearley');
 const rules = require('./rules/rules');
 
-async function main() {
+async function myplToAst() {
   try {
     // create grammar from compiled rules
     const grammar = nearley.Grammar.fromCompiled(rules);
 
-    // get list of input files
-    const inputDirPath = join(__dirname, '..', 'input');
-    const outputDirPath = join(__dirname, '..', 'output');
-    const inputFilesNames = await readdir(inputDirPath);
+    // get list of MyPL files
+    const myplDirPath = join(__dirname, '..', 'mypl');
+    const astDirPath = join(__dirname, '..', 'ast');
+    const myplFilesNames = await readdir(myplDirPath);
 
-    for (const inputFileName of inputFilesNames) {
-      // read input data
-      const inputFilePath = join(inputDirPath, inputFileName);
-      const inputFileData = await readFile(inputFilePath, { encoding: 'utf8' });
+    for (const myplFileName of myplFilesNames) {
+      // read MyPL file data
+      const myplFilePath = join(myplDirPath, myplFileName);
+      const myplFileData = await readFile(myplFilePath, { encoding: 'utf8' });
 
-      // parse data
+      // generate AST
       const parser = new nearley.Parser(grammar);
 
-      parser.feed(inputFileData);
+      parser.feed(myplFileData);
 
-      const abstractSyntaxTree = parser.results[0];
+      const ast = parser.results[0];
 
-      // write output data
-      const outputFileName = basename(inputFileName, '.mypl') + '.ast';
-      const outputFilePath = join(outputDirPath, outputFileName);
-      const outputFileData = JSON.stringify(abstractSyntaxTree, null, '  ') + '\n';
+      // write AST file data
+      const astFileName = basename(myplFileName, '.mypl') + '.ast';
+      const astFilePath = join(astDirPath, astFileName);
+      const astFileData = JSON.stringify(ast, null, '  ') + '\n';
 
-      await writeFile(outputFilePath, outputFileData, { encoding: 'utf-8' });
+      await writeFile(astFilePath, astFileData, { encoding: 'utf-8' });
     }
   } catch (error) {
     console.error(error);
   }
+}
+
+async function astToJs() {
+  try {
+    // get list of AST files
+    const astDirPath = join(__dirname, '..', 'ast');
+    const jsDirPath = join(__dirname, '..', 'js');
+    const astFilesNames = await readdir(astDirPath);
+
+    for (const astFileName of astFilesNames) {
+      // read AST file data
+      const astFilePath = join(astDirPath, astFileName);
+      const astFileData = await readFile(astFilePath, { encoding: 'utf8' });
+      const ast = JSON.parse(astFileData);
+
+      // generate JS
+      const jsCodeLines = generateJsCodeLinesFromAstStatements(ast);
+      const jsFileData = jsCodeLines.join('');
+
+      // write JS file data
+      const jsFileName = basename(astFileName, '.ast') + '.js';
+      const jsFilePath = join(jsDirPath, jsFileName);
+
+      await writeFile(jsFilePath, jsFileData, { encoding: 'utf-8' });
+    }
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+/**
+ * @param {{type: string, [key: string]: any}[]} astStatements
+ * @returns {string[]}
+ */
+function generateJsCodeLinesFromAstStatements(astStatements) {
+  const jsCodeLines = [];
+
+  for (const statement of astStatements) {
+    if (statement.type === 'PrintStatement') {
+      const jsValue = generateJsValueFromExpression(statement.value);
+
+      jsCodeLines.push(`console.log(${jsValue});\n`);
+    } else if (statement.type === 'AssignmentStatement') {
+    } else if (statement.type === 'WhileStatement') {
+    }
+  }
+
+  return jsCodeLines;
+}
+
+/**
+ * @param {{type: string, [key: string]: any}} expression
+ * @returns {string}
+ * */
+function generateJsValueFromExpression(expression) {
+  const operatorsMap = {
+    'PLUS': '+',
+    'MINUS': '-',
+    'TIMES': '*',
+    'DIVIDE': '/',
+    'LESS': '<',
+    'GREATER': '>',
+    'EQUAL': '==',
+    'NOT_EQUAL': '!=',
+    'LESS_OR_EQUAL': '<=',
+    'GREATER_OR_EQUAL': '>=',
+  };
+
+  if (expression.type === 'Literal') {
+    return expression.value;
+  } else if (expression.type === 'Identifier') {
+    return expression.name;
+  } else if (expression.type === 'BinaryExpression') {
+    const jsLeftValue = generateJsValueFromExpression(expression.left);
+    const jsRightValue = generateJsValueFromExpression(expression.right);
+
+    return `${jsLeftValue} ${operatorsMap[expression.operator]} ${jsRightValue}`;
+  }
+}
+
+async function main() {
+  await myplToAst();
+  await astToJs();
 }
 
 main();
