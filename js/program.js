4e9c15e72e0872aa8f54cc00c4075ceb5f73ea84
alex-gudkov <gudkov.bsuir@gmail.com>
2023-09-07 11:33:38 +0300

    compile Fibonacci sequence program example

--- js/program.js
+++ js/program.js
@@ -1,5 +1,11 @@
-let x = 10;
-while (x < 10) {
-  console.log(x);
-  x = x + 1;
+let n = 10;
+let i = 1;
+let x_prev = 1;
+let x_prev_prev = 1;
+while (i <= n) {
+  console.log(x_prev);
+  let x = x_prev + x_prev_prev;
+  x_prev = x_prev_prev;
+  x_prev_prev = x;
+  i = i + 1;
 }
